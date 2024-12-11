ef0aa81b9d9f9803cd952c7ef8b28cf8410a7b91
ShmelevAlexey <tyuiu.shmelevav@outlook.com>
2024-12-12 03:45:28 +0500

    Task6

--- Tyuiu.ShmelevAV.Sprint6.Task6.V13.Lib/DataService.cs
+++ Tyuiu.ShmelevAV.Sprint6.Task6.V13.Lib/DataService.cs
@@ -4,19 +4,19 @@ namespace Tyuiu.ShmelevAV.Sprint6.Task6.V13.Lib
 {
     public class DataService : ISprint6Task6V13
     {
-        public string CollectTextFromFile(string str, string path)
+        public string CollectTextFromFile(string path)
         {
-            str = "";
+            string res = "";
             string[] strPath = File.ReadAllLines(path);
             for (int i = 0; i < strPath.Length; i++)
             {
                 string[] lastStr = strPath[i].Split(' ');
                 if (lastStr.Length >= 2)
                 {
-                    str += lastStr[lastStr.Length - 2];
+                    res += lastStr[lastStr.Length - 2];
                 }
             }
-            return str;
+            return res;
         }
     }
 }
